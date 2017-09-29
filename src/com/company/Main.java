8f84e318928a3265013f778e2a0fb7a31c81454f
RVSpirin <creativniy96@gmail.com>
2017-09-29 08:47:05 +0600

    Go che

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,7 +19,57 @@ public class Main {
 	    System.out.println(Math.min(a,Math.max(b,c)));
 	    System.out.println(Math.max(a,Math.max(b,c)));
 
+        double d = (b*b - 4*a*c);
+        if (a!=0) {
+            if (d < 0) {
+                System.out.println("Корней НЕТ и не будет");
+            }
+            if (d==0) {
+                System.out.println("Корень примерно " + -b/(2*a));
+            }
+            if (d>0) {
+                System.out.println(" Первый корень " + (-b + Math.sqrt(d))/(2*a) + " Второй Корень " + (-b - Math.sqrt(d))/(2*a));
+            }
+        }
+        if (a==0 && b!=0 && c!=0) {
+            System.out.println(-c/b);
+        }
+        if (b==0 && c!=0 && a!=0) {
+            System.out.println(" Первый корень " + (-b + Math.sqrt(d))/(2*a) + " Второй Корень " + (-b - Math.sqrt(d))/(2*a));
+        }
+        if (a==0 && b==0 && c==0) {
+            System.out.println("Корень - любое число");
+        }
 
+        System.out.println("Hello World 2");
+
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        double n = sc.nextDouble();
+
+        if (n==0) {
+            System.out.println("Неа");
+        }
+        if (n < 0) {
+            if (y > x) {
+                c = x;
+                x = y;
+                y = c;
+            }
+            for (double i = x; i >= y; i += n) {
+                System.out.println(Math.sin(i));
+            }
+        }
+            if (n > 0) {
+                if (y < x) {
+                    c = x;
+                    x = y;
+                    y = c;
+                }
+                for (double i = x; i <= y; i += n) {
+                    System.out.println(Math.sin(i));
+                }
+            }
 
     }
 }
